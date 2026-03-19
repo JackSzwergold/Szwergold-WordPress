dd27aec48e5f7e2444ca5be5ebd8a36313087139
Jack Szwergold <szwergoj@newschool.edu>
2026-03-19 14:55:58 -0400

    More work.

--- wp-content/themes/szwergold2026/footer.php
+++ wp-content/themes/szwergold2026/footer.php
@@ -11,37 +11,6 @@
 
 <?php
 
-?>
-
-<div class="container-fluid m-0 p-0">
-  <div class="row m-0 p-0">
-    <div class="col col-12 m-0 p-0 bg-black">
-
-      <div class="container">
-        <div class="row m-0 p-0">
-          <div class="col col-12 m-0 p-0 py-3">
-
-            <!-- Footer BEGIN -->
-            <footer class="user-select-none">
-
-              <span class="text-white">123</span>
-              <span>&nbsp;</span>
-              <span class="text-white">456</span>
-              <span>&nbsp;</span>
-              <span class="text-white">789</span>
-
-            </footer>
-            <!-- Footer END -->
-
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div>
-<?php
-
   /****************************************************************************************/
   // This 'wp_footer' call sets all of the JavaScript and related stuff that WordPress needs to insert in the page.
   wp_footer();
