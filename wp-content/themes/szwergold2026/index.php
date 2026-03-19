953c184d521440e89cad098a31c583c870ad5523
Jack Szwergold <szwergoj@newschool.edu>
2026-03-19 13:15:31 -0400

    More work.

--- wp-content/themes/szwergold2026/index.php
+++ wp-content/themes/szwergold2026/index.php
@@ -1,31 +1,18 @@
 <?php
   get_header();
 ?>
-{% block header %}
-    <header class="col col-12 p-0 m-0 pb-2">
-        <div class="h1 p-0 m-0 text-windsorpro-bold">{% if header.title %}{{ header.title|e }}{% endif %}</div>
-        <div class="h2 p-0 m-0 text-windsorpro-regular">{% if header.description %}{{ header.description }}{% endif %}</div>
-        <hr class="border border-dark border-1 opacity-100">
-    </header>
-{% endblock %}
-
-<!-- {% block header_navigation %}
-    {% include 'partials/navigation.html.twig' %}
-{% endblock %} -->
-
-{% block body %}
-  <main class="col col-12 p-0 m-0">
-    <article class="col col-12 p-0 m-0">
-      <div class="text-georgia-regular">
-        {% block content %}{% endblock %}
-      </div>
-    </article>
-  </main>
-{% endblock %}
-
-{% block bottom %}
-    {{ assets.js('bottom')|raw }}
-{% endblock %}
+<header class="col col-12 p-0 m-0 pb-2">
+  <div class="h1 p-0 m-0 text-windsorpro-bold">{% if header.title %}{{ header.title|e }}{% endif %}</div>
+  <div class="h2 p-0 m-0 text-windsorpro-regular">{% if header.description %}{{ header.description }}{% endif %}</div>
+  <hr class="border border-dark border-1 opacity-100">
+</header>
+<main class="col col-12 p-0 m-0">
+  <article class="col col-12 p-0 m-0">
+    <div class="text-georgia-regular">
+      {% block content %}{% endblock %}
+    </div>
+  </article>
+</main>
 <?php
   get_footer();
 ?>
