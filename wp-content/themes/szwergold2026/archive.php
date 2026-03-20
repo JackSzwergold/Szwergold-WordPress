6c5d2e59e7c947bfa87e72a0d25ac3819a6158d6
Jack Szwergold <szwergoj@newschool.edu>
2026-03-20 14:34:29 -0400

    More work.

--- wp-content/themes/szwergold2026/archive.php
+++ wp-content/themes/szwergold2026/archive.php
@@ -63,7 +63,7 @@
 
 				/********************************************************************************/
 				// Show the title.
-				echo '<div class="h1 p-0 m-0 text-windsorpro-bold">';
+				echo '<div class="h3 p-0 m-0 text-windsorpro-bold">';
 				echo '<a href="' . $permalink . '" rel="bookmark" title="A link to &ldquo;' . $title_attribute . '.&rdquo;" class="text-dark text-decoration-none">';
 				echo $title;
 				echo '</a>';
@@ -72,7 +72,7 @@
 				/********************************************************************************/
 				// Show the author, date and time.
 				if (!empty($the_author)) {
-					echo '<div class="h5 p-0 m-0 text-windsorpro-regular">';
+					echo '<div class="p-0 m-0 text-windsorpro-regular">';
 					echo 'By ' . $the_author;
 					if (!empty($update_date)) {
 						echo ' on ' . $update_date;
