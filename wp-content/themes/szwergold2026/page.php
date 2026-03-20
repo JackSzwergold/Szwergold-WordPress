1d9a0f5a8eef7d06ea6bf90f24a4274aa98a6d13
Jack Szwergold <szwergoj@newschool.edu>
2026-03-20 13:37:03 -0400

    More work.

--- wp-content/themes/szwergold2026/page.php
+++ wp-content/themes/szwergold2026/page.php
@@ -26,13 +26,14 @@
 			// Set the item info variables.
 			$permalink = get_the_permalink();
 			$title = get_the_title();
+			$title_attribute = the_title_attribute(array('echo' => false));
 			$excerpt = get_the_excerpt();
 			$the_author_url = esc_url(get_author_posts_url($authordata->ID, $authordata->user_nicename));
 			$update_date = get_the_time('F j, Y');
 
 			echo '<header class="col col-12 p-0 m-0 pb-2">';
 			echo '<div class="h1 p-0 m-0 text-windsorpro-bold">';
-			echo '<a href="' . $permalink . '" rel="bookmark" title="A link to &ldquo;' . $title . '.&rdquo;" class="text-dark text-decoration-none">';
+			echo '<a href="' . $permalink . '" rel="bookmark" title="A link to &ldquo;' . $title_attribute . '.&rdquo;" class="text-dark text-decoration-none">';
 			echo $title;
 			echo '</a>';
 			echo '</div>';
--- wp-content/themes/szwergold2026/single.php
+++ wp-content/themes/szwergold2026/single.php
@@ -4,164 +4,83 @@
 	// Set the header.
 	get_header();
 
-?>
+	/**********************************************************************/
+	// Set the post content if we have post content.
+	if (have_posts()) {
+		while (have_posts()) {
 
-<header class="p-0 m-0">
-	<div class="p-0 m-0 mx-3">
-		<div class="container m-0 p-0 pb-2 mb-2 border-bottom border-dark">
+			/********************************************************************/
+			// Get the post.
+			the_post();
 
-		<?php
+			global $post;
+			global $authordata;
 
-			/*****************************************************************************************/
-			// Set the template directory and related blog info valiables.
-			$template_directory = get_bloginfo('template_directory');
+			/********************************************************************/
+			// Set the variables.
+			// $the_title_attribute = get_the_title_attribute();
+			$permalink = get_the_permalink();
+			$title = get_the_title();
 
-			if (TRUE && have_posts()) {
+			/********************************************************************************/
+			// Set the item info variables.
+			$permalink = get_the_permalink();
+			$title = get_the_title();
+			$title_attribute = the_title_attribute(array('echo' => false));
+			$excerpt = get_the_excerpt();
+			$the_author_url = esc_url(get_author_posts_url($authordata->ID, $authordata->user_nicename));
+			$update_date = get_the_time('F j, Y');
 
-				while (have_posts()) {
-
-					the_post();
-
-					if (is_home()) {
-						if (function_exists('wp_list_comments')) {
-							post_class();
-						} // if
-					} // if
-
-					global $post;
-					global $authordata;
-
-					/********************************************************************************/
-					// Set the main post ID.
-					$main_post_ID_array = array();
-					$main_post_ID_array[] = get_the_ID();
-
-					/********************************************************************************/
-					// Set the blog post info valiables.
-					$story_author_id = $post->post_author;	//	important for sidebar!
-					$permalink = get_the_permalink();
-					$title = get_the_title();
-					$title_attribute = the_title_attribute(array('echo' => false));
-					$the_author = get_the_author();
-					$the_author_url = esc_url(get_author_posts_url($authordata->ID, $authordata->user_nicename));
-		            $display_date = get_the_time('F j, Y');
-		            $address = get_post_meta($post->ID, 'address', true);
-
-					echo '<div class="h2 p-0 m-0 fw-bold">';
-					echo '<a href="' . $permalink . '" title="Permanent Link to ' . $title_attribute . '" class="text-decoration-none text-dark">';
-					echo $title;
-					echo '</a>';
-					echo '</div>';
-
-					echo '<div class="p-0 m-0">';
-					echo 'By ';
-					echo '<a href="' . $the_author_url . '" title="Posts by ' . $the_author . '" class="text-decoration-none text-dark">';
-					echo $the_author;
-					echo '</a>';
-					echo '</div>';
-
-					echo '<div class="text-secondary p-0 m-0">';
-					echo $display_date;
-					echo '</div>';
-
-					// Set the categories array.
-					$categories_array = array();
-					$categories = get_the_category();
-					foreach ($categories AS $value) {
-						if ($value->slug == 'featured') {
-							continue;
-						} // if
-						$category_link = get_category_link($value->term_id);
-						$categories_array[] =
-								'<a href="' . $category_link . '" title="View all the stories from ' . $value->name . '" class="text-dark">' 
-							. $value->name
-							. '</a>'
-							;
-					} // foreach
-
-					echo '<div class="text-secondary p-0 m-0">';
-					echo $address;
-					echo '</div>';
-					echo '<div class="text-secondary">';
-					echo 'Neighborhood: ';
-					echo implode(', ', $categories_array);
-					echo '</div>';
-
-			?>
-
-		</div>
-	</div>
-</header>
-
-<main class="p-0 m-0">
-	<div class="p-0 m-0 mx-3">
-
-		<?php
-
-				/************************************************************************************/
-				// Display the main entry.
-				echo '<div class="container m-0 p-0 mb-2 ">';
-				echo '<div class="row m-0 p-0">';
-				echo '<div class="col col-12 col-lg-8 m-0 p-0 mbn_body_text">';
-				the_content('&raquo; Read the rest of the story &laquo;');
-				echo '</div>';
-				echo '</div>';
-				echo '</div>';
-
-				/************************************************************************************/
-				// Display the tags.
-				wp_link_pages(array('before' => '<p>Page: ', 'after' => '</p>', 'next_or_number' => 'number'));
-				ob_start();
-				the_tags('<span class="fw-bold">Tagged:</span> ', ', ', '');
-				$the_tags = ob_get_contents();
-				ob_end_clean();
-				if (!empty($the_tags)) {
-					$the_tags = str_replace('<a href=', '<a class="text-dark" href=', $the_tags);;
-					echo '<div class="container m-0 p-0 pb-2 mb-2">';
-					echo '<div class="row m-0 p-0">';
-					echo '<div class="col col-12 m-0 p-0">';
-					echo $the_tags;
-					echo '</div>';
-					echo '</div>';
-					echo '</div>';
-				} // if
-
-				echo '</div>';
-				echo '</div>';
-				echo '</div>';
-
-				break;
-
-			} // while
-		}
-		else {
-
-			echo '<div class="h1 p-0 m-0 fw-bold">';
-			echo 'Whoops!';
+			echo '<header class="col col-12 p-0 m-0 pb-2">';
+			echo '<div class="h1 p-0 m-0 text-windsorpro-bold">';
+			echo '<a href="' . $permalink . '" rel="bookmark" title="A link to &ldquo;' . $title_attribute . '.&rdquo;" class="text-dark text-decoration-none">';
+			echo $title;
+			echo '</a>';
 			echo '</div>';
+			if (!empty($excerpt)) {
+				echo '<div class="h2 p-0 m-0 text-windsorpro-regular">';
+				echo $excerpt;
+				echo '</div>';				
+			} // if
+			echo '<hr class="border border-dark border-1 opacity-100">';
+			echo '</header>';
 
-		?>
+			echo '<main class="col col-12 p-0 m-0">';
+			echo '<article class="col col-12 p-0 m-0">';
+			echo '<div class="text-georgia-regular">';
 
-		</div>
-	</div>
-</header>
+			/********************************************************************/
+			// Grab the content into a variable.
+			ob_start();
+			the_content('Read the rest of this entry &raquo;');
+			$the_content = ob_get_contents();
+			ob_end_clean();
 
-<main class="p-0 m-0">
-	<div class="p-0 m-0 mx-3">
+			/********************************************************************/
+			// Output the content.
+			echo $the_content;
 
-	<?php
+			echo '</div>';
+			echo '</article>';
+			echo '</main>';
 
-		echo '<p>';
-		echo "Something wrong.";
-		echo '</p>';
+			/********************************************************************/
+			// Grab the content into a variable.
+			ob_start();
+			edit_post_link();
+			$edit_post_link = ob_get_contents();
+			ob_end_clean();
+
+		} // while
+
+	} // if
+	else {
+
+		echo '<div>';
+		echo "<p>Nothing.</p>";
+		echo '</div>';
 
 	} // else
-	?>
-
-	</div>
-</main>
-
-<?php
 
 	/**********************************************************************/
 	// Set the sidebar.
