9f0c6aff1600743ed144776fbf2ffd05943ab09b
Jack Szwergold <szwergoj@newschool.edu>
2026-03-20 14:45:07 -0400

    More work.

--- wp-content/themes/szwergold2026/single.php
+++ wp-content/themes/szwergold2026/single.php
@@ -42,7 +42,7 @@
 
 			/********************************************************************************/
 			// Show the excerpt.		
-			if (!empty($excerpt)) {
+			if (FALSE && !empty($excerpt)) {
 				echo '<div class="h2 p-0 m-0 text-windsorpro-regular">';
 				echo $excerpt;
 				echo '</div>';				
