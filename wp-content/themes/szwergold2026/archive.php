f847ca5e82fca07ff1cbfc691b13131f524aa5ae
Jack Szwergold <szwergoj@newschool.edu>
2026-03-27 21:31:09 -0400

    More work.

--- wp-content/themes/szwergold2026/archive.php
+++ wp-content/themes/szwergold2026/archive.php
@@ -359,7 +359,6 @@
 		$category_block = implode('', $temp);
 		if (!empty($category_name)) {
 			$category_block  =
-			      // '<div class="col col-12 p-0 m-0 mb-3 border-bottom border-dark px-2 py-1 mb-2 bg-warning">'
 			      '<div class="col col-12 p-0 m-0 mb-3 border-bottom border-dark">'
 				. '<div class="h3 text-windsorpro-bold col col-12 p-0 m-0 mb-1">'
 				. '<a href="' . $category_link . '">'
