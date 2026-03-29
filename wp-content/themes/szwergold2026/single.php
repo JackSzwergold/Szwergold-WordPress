44b14b51e8924f7129f9ff649d89357b6336ac15
Jack Szwergold <szwergoj@newschool.edu>
2026-03-29 19:06:18 -0400

    More work.

--- wp-content/themes/szwergold2026/single.php
+++ wp-content/themes/szwergold2026/single.php
@@ -42,8 +42,7 @@
 			// Set the text CSS.
 			$text_css = null;
 			if (!empty($page_category_slug) && in_array($page_category_slug, array('notes'))) {
-				$text_css = 'display-6';
-				// $text_css = 'h5';
+				$text_css = 'fs-3';
 			} // if
 
 			/**********************************************************************/
