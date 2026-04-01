8d36b398f09aa4583c670f991dee7c19a043a591
Jack Szwergold <szwergoj@newschool.edu>
2026-04-01 15:28:50 -0400

    More work.

--- wp-content/themes/szwergold2026/functions.php
+++ wp-content/themes/szwergold2026/functions.php
@@ -100,64 +100,64 @@ function szwergold_widgets_init() {
 		'name'          => __('Header 1'),
 		'id'            => 'header-1',
 		'description'   => __('Add widgets here to appear on your page header.', 'szwergold'),
-		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3"><div class="p-0 m-0 text-georgia-regular">',
-		'after_widget'  => '</div></div>',
 		'before_title'  => '<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark">',
 		'after_title'   => '</div>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3"><div class="p-0 m-0 text-georgia-regular">',
+		'after_widget'  => '</div></div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Header 2'),
 		'id'            => 'header-2',
 		'description'   => __('Add widgets here to appear on your page header.', 'szwergold'),
-		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3"><div class="p-0 m-0 px-2 text-georgia-regular">',
-		'after_widget'  => '</div></div>',
 		'before_title'  => '<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark">',
 		'after_title'   => '</div>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3"><div class="p-0 m-0 px-2 text-georgia-regular">',
+		'after_widget'  => '</div></div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Footer 1'),
 		'id'            => 'widget-footer-1',
 		'description'   => __('Add widgets here to appear on your page footer.', 'szwergold'),
-		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
-		'after_widget'  => '</div>',
 		'before_title'  => '<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark">',
 		'after_title'   => '</div>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
+		'after_widget'  => '</div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Footer 2'),
 		'id'            => 'widget-footer-2',
 		'description'   => __('Add widgets here to appear on your page footer.', 'szwergold'),
-		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
-		'after_widget'  => '</div>',
 		'before_title'  => '<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark">',
 		'after_title'   => '</div>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
+		'after_widget'  => '</div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Home Featured 1'),
 		'id'            => 'widget-home-featured-1',
 		'description'   => __('Add widgets here to appear on your homepage.', 'szwergold'),
+		'before_title'  => '<div class="h3 text-railroadgothic text-darkblue text-decoration-none col col-12 p-0 m-0 mb-1">',
+		'after_title'   => '</div>',
 		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<div class="h3 text-railroadgothic col col-12 p-0 m-0 mb-1">',
-		'after_title'   => '</div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Home Featured 2'),
 		'id'            => 'widget-home-featured-2',
 		'description'   => __('Add widgets here to appear on your homepage.', 'szwergold'),
+		'before_title'  => '<div class="h3 text-railroadgothic text-darkblue text-decoration-none col col-12 p-0 m-0 mb-1">',
+		'after_title'   => '</div>',
 		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<div class="h3 text-railroadgothic col col-12 p-0 m-0 mb-1">',
-		'after_title'   => '</div>',
 	));
 	register_sidebar(array(
 		'name'          => __('Home Featured 3'),
 		'id'            => 'widget-home-featured-3',
 		'description'   => __('Add widgets here to appear on your homepage.', 'szwergold'),
+		'before_title'  => '<div class="h3 text-railroadgothic text-darkblue text-decoration-none col col-12 p-0 m-0 mb-1">',
+		'after_title'   => '</div>',
 		'before_widget' => '<div id="%1$s" class="widget %2$s col col-12 pe-md-3">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<div class="h3 text-railroadgothic col col-12 p-0 m-0 mb-1">',
-		'after_title'   => '</div>',
 	));
 } // szwergold_widgets_init
 add_action('widgets_init', 'szwergold_widgets_init');
