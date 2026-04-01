df7ca3c0223e1e6a1d4f82a1c5dd3df27844fb07
Jack Szwergold <szwergoj@newschool.edu>
2026-04-01 12:57:56 -0400

    More work.

--- wp-content/themes/szwergold2026/page.php
+++ wp-content/themes/szwergold2026/page.php
@@ -89,41 +89,8 @@
 
         /**********************************************************************************/
         // Init variables.
-        $home_latest = null;
-        $home_featured_main = null;
         $home_featured_1 = null;
         $home_featured_2 = null;
-
-        /**********************************************************************************/
-        // Main featured stuff.
-        if (is_active_sidebar('home-featured-main')) {
-
-          /********************************************************************************/
-          // Grab the 'main-featured' into a variable.
-          ob_start();
-          dynamic_sidebar('home-featured-main');
-          $home_featured_main = ob_get_contents();
-          ob_end_clean();
- 
-        } // if
-
- 
-        /**********************************************************************************/
-        // Post sidebar 1 stuff.
-        if (is_active_sidebar('home-latest')) {
-
-          /********************************************************************************/
-          // Capture the content and set it in a variable so we can tweak the links.
-          ob_start();
-          dynamic_sidebar('home-latest');
-          $home_latest = ob_get_contents();
-          ob_end_clean();
-
-          /********************************************************************************/
-          // Filter the content in the variable.
-          // $home_latest = str_replace('<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark"><a href=', '<div class="widget-title h5 text-capitalize p-0 m-0 pb-1 mb-2 border-bottom border-dark"><a class="text-decoration-none text-dark" href=', $home_latest);
-
-        } // if
 
         /**********************************************************************************/
         // Home featured 1 stuff.
@@ -162,24 +129,20 @@
 ?>
 
 		<div class="row p-0 m-0 me-xl-3">
-		  <div class="col col-12 col-xl-3 m-0 p-0 mb-3 order-2 order-xl-first">
-		    <div class="container">
-		      <div class="row"> 
-		        <?php
-		          echo $home_latest;
-		        ?>
-		      </div>
-		    </div>
-		  </div>
-		  <div class="col col-12 col-xl-9 m-0 p-0 mb-3 order-1 order-xl-last">
-		    <div class="container">
-		      <div class="row"> 
-		        <?php
-		          echo $home_featured_main;
-		        ?>
-		      </div>
-		    </div>
-		  </div>
+			<div class="col col-12 col-xl-3 m-0 p-0 mb-3 order-2 order-xl-first">
+				<div class="container">
+					<div class="row"> 
+					One
+					</div>
+				</div>
+			</div>
+			<div class="col col-12 col-xl-9 m-0 p-0 mb-3 order-1 order-xl-last">
+				<div class="container">
+					<div class="row"> 
+					Two
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="row p-0 m-0 me-xl-3">
