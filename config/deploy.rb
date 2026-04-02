09d96b4ac2105d2a9b82de97555914e94f25eb02
Jack Szwergold <szwergoj@newschool.edu>
2026-04-02 14:25:06 -0400

    Work on the Capistrano stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,18 @@
 # config valid only for current version of Capistrano
 lock ['>= 3.17.0', '<= 3.20.0']
 
-set :application, 'szwergold.com-TEST'
+set :application, 'szwergold.com'
 set :short_name, 'szwergold.com'
 set :repo_url, 'git@github.com:JackSzwergold/Szwergold-WordPress.git'
+
+# Default value for :scm is :git
+# set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
 
 # Default value for :log_level is :debug
-set :log_level, :info
+set :log_level, :debug
 
 # Default value for :pty is false
 set :pty, false
@@ -19,12 +22,15 @@ set :pty, false
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('wp-content/uploads', 'wp-content/upgrade')
+set :linked_dirs, fetch(:linked_dirs, []).push('cache')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Disable warnings about the absence of the stylesheets, javscripts & images directories.
+# Default value for keep_releases is 5
+set :keep_releases, 3
+
+# Disable warnings about the absence of the styleseheets, javscripts & images directories.
 set :normalize_asset_timestamps, false
 
 # Set the root deployment path.
@@ -50,19 +56,9 @@ namespace :deploy do
     end
   end
 
-  # Echo the current path to a file. Needed for WordPress deployments.
-  desc "Echo the current path."
-  task :echo_current_path do
-    on roles(:app) do
-
-        execute "echo #{release_path} > #{release_path}/CURRENT_PATH"
-
-    end
-  end
-
-  # Create the 'create_symlinks' task to create symbolic links and other related items.
+  # Create the 'create_symlink' task to create symbolic links and other related items.
   desc "Set the symbolic links."
-  task :create_symlinks do
+  task :create_symlink do
     on roles(:app) do
 
       # info "If there is no directory & no symbolic link to '#{fetch(:parent_site_path)}' then create a directory named '#{fetch(:parent_site_path)}'."
@@ -73,28 +69,6 @@ namespace :deploy do
 
       # info "If there is a symbolic link to '#{fetch(:parent_site_path)}/#{fetch(:short_name)}', delete it. Irregardless, create a new symbolic link to '#{fetch(:parent_site_path)}/#{fetch(:short_name)}'."
       execute "cd #{fetch(:live_root)} && if [ -h #{fetch(:parent_site_path)}/#{fetch(:short_name)} ]; then rm #{fetch(:parent_site_path)}/#{fetch(:short_name)}; fi && ln -sf #{current_path} ./#{fetch(:parent_site_path)}/#{fetch(:short_name)}"
-
-    end
-  end
-
-  # Remove repository cruft from the deployment.
-  desc "Remove cruft from the deployment."
-  task :remove_cruft do
-    on roles(:app) do
-
-      # Remove files and directories that aren’t needed on a deployed install.
-      execute "cd #{current_path} && if [ -f robots.txt ]; then mv -f robots.txt robots.temp; fi && rm -rf {development_dbs,config,Capfile,*.html,*.txt,*.md,*.sql,.gitignore} && if [ -f 'robots.temp' ]; then mv -f robots.temp robots.txt; fi"
-
-    end
-  end
-
-  # Set group write permissions to the deployment.
-  desc "Set group write permissions to the deployment."
-  task :set_group_write do
-    on roles(:app) do
-
-      # Set group write permissions to the deployment.
-      execute "cd #{current_path} && chmod -R g+w ."
 
     end
   end
