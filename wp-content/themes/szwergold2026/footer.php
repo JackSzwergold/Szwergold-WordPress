f60a57edb2027d1fcca837d3bf90ff1169a1db53
Jack Szwergold <szwergoj@newschool.edu>
2026-04-03 14:12:56 -0400

    More work.

--- wp-content/themes/szwergold2026/footer.php
+++ wp-content/themes/szwergold2026/footer.php
@@ -325,10 +325,10 @@
   // Set the footer array items.
   foreach ($footer_items_array as $key => $value) {
     $footer_items_array[$key] =
-        '<li class="list-inline-item fw-normal text-nowrap p-0 m-0">'
-      . '<em>'
+        '<li class="list-inline-item text-nowrap p-0 m-0">'
+      . '<span class="small fst-italic">'
       . $value
-      . '</em>'
+      . '</span>'
       . '</li>'
       ;
   } // foreach
