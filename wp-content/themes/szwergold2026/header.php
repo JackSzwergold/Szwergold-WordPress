75666232de9e46c49e18d9dff4683f3e60b4c554
Jack Szwergold <szwergoj@newschool.edu>
2026-04-03 14:58:59 -0400

    More work.

--- wp-content/themes/szwergold2026/header.php
+++ wp-content/themes/szwergold2026/header.php
@@ -227,13 +227,14 @@
 </head>
 <body id="top" class="p-0 m-0 <?php echo $page_slugs_string; ?>">
 
-	<nav class="header p-0 m-0 px-2 pt-2 pb-3 bg-darkblue">
-		<div class="sticky-top col col-12 p-0 m-0 px-3 bg-darkblue order-2 order-xl-first">
+	<!-- <nav class="header p-0 m-0 px-2 pt-2 pb-3 bg-darkblue"> -->
+	<nav class="header p-0 m-0 px-2 bg-darkblue">
+		<div class="sticky-top col col-12 p-0 m-0 pt-2 px-3 bg-darkblue z-1">
 			<?php
 				echo $blog_name;
 			?>
 		</div>
-		<div class="sticky-top col col-12 p-0 m-0 px-3 bg-darkblue order-1 order-xl-last">
+		<div class="sticky-top col col-12 p-0 m-0 pb-3 px-3 bg-darkblue z-0">
 			<?php
 				echo $blog_description;
 			?>
